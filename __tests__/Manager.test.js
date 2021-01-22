8d7d773fc5e2709b32fdb063d697e3d3cfe44834
daniela <daniela.acuna93@gmail.com>
2021-01-22 00:34:54 -0600

    manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,26 @@
-const Manager = require ('/lib/Manager');
+const Manager = require ('../lib/Manager.js');
+
+
+test('creates a new manager object', () => {
+    const manager = new Manager('Dave','45','davey@gmail.com','222');
+    expect(manager.name).toBe('Dave');
+  
+  });
+  
+  test('returns id number of Manager', () => {
+    const manager = new Manager('Dave','45','davey@gmail.com','222');
+   expect(manager.id).toBe('45');
+  
+  
+  });
+  
+  test('return email address of Manager', () => {
+    const manager = new Manager('Dave','45','davey@gmail.com','222');
+   expect(manager.email).toBe('davey@gmail.com');
+  
+  });
+  test('return office number of Manager', () => {
+    const manager = new Manager('Dave','45','davey@gmail.com','222');
+   expect(manager.office).toBe('222');
+  
+  });
