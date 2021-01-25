fea6d31c76598811b88d29d065394273ca53bedc
daniela <daniela.acuna93@gmail.com>
2021-01-25 01:15:43 -0600

    edits to index.js

--- Index.js
+++ Index.js
@@ -1,39 +1,69 @@
-const fs = require("fs");
-const path = require("path");
+const fs= require('fs');
+
+
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
 
 
-const promptUser = () => {
-return inquirer.prompt([
+const promptUser = [];
 
-    {
+const questions= 
+   inquirer.prompt([
+  {
+
+  ///Manager// 
+
       type: 'input',
-      name: 'Name ',
-      message: 'What is the Managers name?'
+      name: 'Name',
+      message: 'What is the Managers name?',
+      validate: nameInput => {
+         if (nameInput){
+           return true;
+
+         }else {
+           console.log('Please enter Managers name');
+           return false;
+         }
+        }
+        },
+        {
+   
+          type:'input',
+          name: 'Id',
+          message: 'What is the Managers Id?',
+          validate: idInput => {
+            if (idInput) {
+              return true;
+            } else {
+              console.log('You must enter an ID number!');
+              return false;
+            }
+    
+          
+          }
+  
     },
     {
-      type: 'input',
-      name: 'Email',
-      message: 'Provide Managers email'
-    },
-    {
-      type: 'input',
-      name: 'ID number ',
-      message: 'Manager ID',
-      choices: ['25']
-    }
-]);
-}
-
-
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    // const pageHTML = generatePage();
-
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
-  });
+   
+      type:'input',
+      name: 'email',
+      message: 'What is the Managers email?',
+      validate: emailInput => {
+        if(emailInput){
+        return true;
+        } else {
+          console.log('You must enter a valid email address!');
+        }
+      }
+        
+      
+      },
+      {
+     
+        type:'input',
+        name: 'office',
+        message: 'What is the Managers office number?',
+      }
+    ])
