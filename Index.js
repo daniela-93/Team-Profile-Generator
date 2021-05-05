42358cb0f4433ec6581bd3bc2116d9b7f6c7ba06
daniela <daniela.acuna93@gmail.com>
2021-05-04 23:37:12 -0500

    adjustments to index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,10 @@
-
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateCards = require("./src/generated-page");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+  
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Manager = require('./lib/Manager.js');
+const Intern = require('./lib/Intern.js');
 
 const employees = [];
 
@@ -54,20 +54,7 @@ const promptUser = () => {
             }
         }
       },
-      {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter a phone number to reach this person: (Required)',
-        validate: idInput => {
-          if (idInput){
-                return true;
-          }
-          else{
-            console.log('Please enter a valid ID!');
-            return false;
-          }
-        }
-      },
+      
       {
         type: 'confirm',
         name: 'confirmAddEmployee',
@@ -144,9 +131,131 @@ const promptEngineer = () =>{
         default: false
       }
   ])
-
+//Intern//
 }
 const promptIntern = () =>{
   return inquirer.prompt([
   {
+    type: 'input',
+    name: 'name',
+    message: 'What is the interns name? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your interns name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'Enter this persons ID number (Required)',
+    validate: idInput => {
+      if (idInput){
+        return true;
+      }
+      else{
+        console.log('Please enter a valid ID!');
+        return false;
+      }
+    }
+  },
+  {
+      message: "Please enter your email",
+      name: "email",
+      type: "input",
+      default: () => {},
+      validate: function (email) {
+
+          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+          if (valid) {
+              return true;
+          } else {
+              console.log(".  Please enter a valid email")
+              return false;
+          }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What school is the Intern attending: (Required)',
+      validate: idInput => {
+        if (idInput){
+              return true;
+        }
+        else{
+          console.log('Please enter a valid school!');
+          return false;
+        }
+      }
+    },
     
+      {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: 'Would you like to enter another employee?',
+        default: false
+      }
+    ])
+}
+const promptAddEmployee = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'Select the additional employees role? (Required)',
+      choices: ['Intern', 'Engineer']  
+    }
+  ])
+  .then(employeeData =>{
+      if(employeeData.role == "Intern"){
+          promptIntern()
+          .then(internData =>{
+              const intern1 = new Intern(internData.name, internData.id, internData.email, internData.school);
+              employees.push(intern1);
+              if(internData.confirmAddEmployee){
+                  promptAddEmployee()
+              } else{
+                writeToFile(employees);
+                console.log("Checkout the generated page from the dist folder!");
+              }
+          })
+      }
+      else{
+          promptEngi()
+          .then(engiData =>{
+              const engi1 = new Engineer(engiData.name, engiData.id, engiData.email, engiData.gitHub);
+              employees.push(engi1);
+              if(engiData.confirmAddEmployee){
+                  promptAddEmployee()
+              } else{
+                writeToFile(employees);
+                console.log("Checkout the generated page from the dist folder!");
+              }
+          })
+      }
+  })
+}
+const generatePage = require('./src/page-template.js');
+
+const writeToFile = function(passedData){
+fs.writeFile('./dist/GeneratedPage.html', generatePage(employees), err =>{
+    if(err) throw err;
+    console.log('Page complete! Check out GeneratedPage.html to see the output!');
+})
+}
+promptUser()
+  .then(memberData => {
+    const manager1 = new Manager(memberData.name, memberData.id, memberData.email, memberData.officeNumber);
+    employees.push(manager1);
+    if(memberData.confirmAddEmployee){
+        promptAddEmployee()
+    } else{
+        writeToFile(employees);
+        console.log("Checkout the generated page from the dist folder!");
+    }
+  })
